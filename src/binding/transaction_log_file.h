5c737640936964e848871714ff813d9193477a1e
Chris Barber <chris@harperdb.io>
2025-10-07 12:52:03 -0500

    read/write data

--- src/binding/transaction_log_file.h
+++ src/binding/transaction_log_file.h
@@ -54,8 +54,8 @@ struct TransactionLogFile final {
 
 	void close();
 	void open();
-	ssize_t readBytes(void* buffer, size_t size, off_t offset = -1);
-	ssize_t writeBytes(const void* buffer, size_t size, off_t offset = -1);
+	ssize_t readData(void* buffer, size_t size, off_t offset = -1);
+	ssize_t writeData(const void* buffer, size_t size, off_t offset = -1);
 };
 
 } // namespace rocksdb_js
