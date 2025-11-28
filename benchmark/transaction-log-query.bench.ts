148e32db0f2f320d6f8ff5d5504f7c2276abaea0
Kris Zyp <kriszyp@gmail.com>
2025-11-28 06:29:41 -0700

    Update benchmark

--- benchmark/transaction-log-query.bench.ts
+++ benchmark/transaction-log-query.bench.ts
@@ -6,6 +6,7 @@ import { TransactionLogReader } from '../src';
 describe('transaction-log-query', () => {
 	const SMALL_DATASET = 100;
 	const smallDataset = generateTestData(SMALL_DATASET, 20, 100);
+	const ENTRY_COUNT = 1000;
 
 	describe('small log', () => {
 		describe('log with 10 100 byte records', () => {
@@ -14,33 +15,35 @@ describe('transaction-log-query', () => {
 				async setup(ctx) {
 					const db = ctx.db;
 					const log = db.useLog('0');
-					global.queryLog = ctx.queryLog = new TransactionLogReader(log);
+					ctx.log = log;
 					const value = Buffer.alloc(100, 'a');
 					ctx.start = Date.now();
-					for (let i = 0; i < 40; i++) {
+					for (let i = 0; i < ENTRY_COUNT; i++) {
 						await db.transaction(async (txn) => {
 							log.addEntry(value, txn.id);
 						});
-						await new Promise(resolve => setTimeout(resolve, 10));
+						await new Promise(resolve => setTimeout(resolve, 1));
 					}
 					ctx.duration = Date.now() - ctx.start;
 				},
-				async bench({ db, data, queryLog, start, duration }) {
-					let result = Array.from(queryLog.query({start: start + Math.random() * duration}));
+				async bench({ db, data, log, start, duration }) {
+					let result = Array.from(log.query({start: start + Math.random() * duration}));
 				}
 			});
 
 			benchmark('lmdb', {
-				setup(ctx) {
+				async setup(ctx) {
 					let start = Date.now();
 					const value = Buffer.alloc(100, 'a');
-					for (let i = 0; i < 40; i++) {
+					ctx.start = start;
+					for (let i = 0; i < ENTRY_COUNT; i++) {
 						ctx.db.putSync((start + i) as unknown as string, value);
 					}
+					ctx.duration = ENTRY_COUNT;
 				},
-				async bench({ db, data }) {
+				async bench({ db, data, start, duration }) {
 					// @ts-ignore
-					let result = Array.from(db.getRange({}));
+					let result = Array.from(db.getRange({start: start + Math.random() * duration}));
 				}
 			});
 		});
