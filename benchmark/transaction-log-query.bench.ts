25e8a73a455a7cc6bb47c2031458cb0590164854
Kris Zyp <kriszyp@gmail.com>
2025-11-29 06:11:25 -0700

    More benchmarks

--- benchmark/transaction-log-query.bench.ts
+++ benchmark/transaction-log-query.bench.ts
@@ -2,15 +2,79 @@ import { describe } from 'vitest';
 import { benchmark, generateRandomKeys, generateTestData } from './setup.js';
 import { ABORT } from 'lmdb';
 import { TransactionLogReader } from '../src';
+const CONCURRENCY_SLOWDOWN = 100;
 
 describe('transaction-log-query', () => {
 	const SMALL_DATASET = 100;
 	const smallDataset = generateTestData(SMALL_DATASET, 20, 100);
 	const ENTRY_COUNT = 1000;
+	const data = Buffer.alloc(100, 'a');
 
-	describe('small log', () => {
-		describe('log with 10 100 byte records', () => {
+	describe('transaction log', () => {
+		describe('write log with 100 byte records', () => {
+			benchmark('rocksdb', {
+				async setup(ctx) {
+					const db = ctx.db;
+					const log = db.useLog('0');
+					ctx.log = log;
+				},
+				bench: concurrent(({ db, value, log, start, duration },  outstanding) => {
+					return db.transaction((txn) => {
+						log.addEntry(data, txn.id);
+					});
+				}),
+			});
 
+			benchmark('lmdb', {
+				async setup(ctx) {
+					let start = Date.now();
+					ctx.index = start;
+				},
+				bench: concurrent((ctx, outstanding) => {
+					const { db } = ctx;
+					return db.put(ctx.index++, data);
+				}),
+			});
+		});
+		describe('read 100 iterators while write log with 100 byte records', () => {
+			benchmark('rocksdb', {
+				async setup(ctx) {
+					const db = ctx.db;
+					const log = db.useLog('0');
+					ctx.log = log;
+					ctx.iterators = Array(100).fill(null).map(() => log.query({ start: 1 }));
+				},
+				bench: concurrent(({ db, iterators, log },  outstanding) => {
+					let last;
+					for (let iterator of iterators) {
+						for (let entry of iterators) {
+							last = entry.timestamp;
+						}
+					}
+					return db.transaction((txn) => {
+						log.addEntry(data, txn.id);
+					});
+				}),
+			});
+
+			benchmark('lmdb', {
+				async setup(ctx) {
+					let start = Date.now();
+					ctx.index = start;
+					ctx.last = 0;
+				},
+				bench: concurrent((ctx, outstanding) => {
+					const { db, last } = ctx;
+					for (let i = 0; i < 100;i ++) {
+						for (let { key, value } of db.getRange({ start: ctx.last })) {
+							ctx.last = key + 1;
+						}
+					}
+					return db.put(ctx.index++, data);
+				}),
+			});
+		});
+		describe('read randomly from log with 1000 100 byte records', () => {
 			benchmark('rocksdb', {
 				async setup(ctx) {
 					const db = ctx.db;
@@ -49,3 +113,17 @@ describe('transaction-log-query', () => {
 		});
 	});
 });
+function concurrent<C>(execute: (ctx: C) => Promise<void>, concurrencySlowdown = CONCURRENCY_SLOWDOWN): (ctx: C) => Promise<void> {
+	let outstanding = 0;
+	let rejection;
+	return (ctx: C) => {
+		outstanding++;
+		execute(ctx, outstanding).then(() => outstanding--, (error) => { rejection = error });
+		if (rejection) {
+			let error = rejection;
+			rejection = null;
+			throw rejection;
+		}
+		return new Promise(outstanding > concurrencySlowdown ? (resolve) => setTimeout(resolve, concurrencySlowdown - outstanding) : setImmediate) as Promise<void>;
+	}
+}
