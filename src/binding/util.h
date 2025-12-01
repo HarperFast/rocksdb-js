9904f354bce28506315e76b6188c7300803c7535
Chris Barber <chris@harperdb.io>
2025-12-01 12:27:29 -0600

    Revert to memcpy

--- src/binding/util.h
+++ src/binding/util.h
@@ -543,16 +543,17 @@ inline uint8_t readUint8(const char* buffer) {
 
 inline void writeDoubleBE(char* buffer, double value) {
 	// Interpret the double's bits as uint64_t and write in big endian
-	union { double d; uint64_t u; } converter;
-	converter.d = value;
-	writeUint64BE(buffer, converter.u);
+	uint64_t bits;
+	std::memcpy(&bits, &value, sizeof(double));
+	writeUint64BE(buffer, bits);
 }
 
 inline double readDoubleBE(const char* buffer) {
 	// Read uint64_t in big endian and interpret as double
-	union { double d; uint64_t u; } converter;
-	converter.u = readUint64BE(buffer);
-	return converter.d;
+	uint64_t bits = readUint64BE(buffer);
+	double value;
+	std::memcpy(&value, &bits, sizeof(double));
+	return value;
 }
 
 /**
