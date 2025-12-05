8423668d5085ac4da1c2e16b302c0628c5f6ff62
Kris Zyp <kriszyp@gmail.com>
2025-12-04 22:33:25 -0700

    Update src/binding/transaction_log_file_posix.cpp
    Co-authored-by: Chris Barber <chris@harperdb.io>

--- src/binding/transaction_log_file_posix.cpp
+++ src/binding/transaction_log_file_posix.cpp
@@ -64,7 +64,7 @@ void TransactionLogFile::openFile() {
 }
 
 MemoryMap* TransactionLogFile::getMemoryMap(uint32_t fileSize) {
-	if (!memoryMap) {
+	if (!this->memoryMap) {
 		void* map = ::mmap(NULL, fileSize, PROT_READ, MAP_SHARED, this->fd, 0);
 		DEBUG_LOG("%p TransactionLogFile::getMemoryMap new memory map: %p\n", this, map);
 		if (map == MAP_FAILED) {
