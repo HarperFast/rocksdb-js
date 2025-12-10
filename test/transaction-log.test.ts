6be6cf88764cc0dea626aef933acd096314ddfd1
Chris Barber <chris@harperdb.io>
2025-12-10 15:26:30 -0600

    Isolate test

--- test/transaction-log.test.ts
+++ test/transaction-log.test.ts
@@ -114,7 +114,7 @@ describe('Transaction Log', () => {
 			expect(sequenceNumber).toBe(1);
 
 			const buffer = log._getMemoryMapOfFile(1);
-			expect(buffer.slice(0, 4).toString()).toBe('WOOF');
+			expect(buffer.subarray(0, 4).toString()).toBe('WOOF');
 		}));
 	});
 
@@ -139,7 +139,7 @@ describe('Transaction Log', () => {
 			expect(queryResults.length).toBe(1);
 		}));
 
-		it('should query a transaction log with different options', () => dbRunner(async ({ db }) => {
+		it.only('should query a transaction log with different options', () => dbRunner(async ({ db }) => {
 			const log = db.useLog('foo');
 			const value = Buffer.alloc(10, 'a');
 			for (let i = 0; i < 5; i++) {
