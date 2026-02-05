f95801aa07900ab278c3a417d7d720ea8035a3fe
Kris Zyp <kriszyp@gmail.com>
2026-02-05 08:52:37 -0700

    Try throwing an error if no store, and initialize the log position

--- test/transaction-log.test.ts
+++ test/transaction-log.test.ts
@@ -127,7 +127,7 @@ describe('Transaction Log', () => {
 	});
 
 	describe('Transaction log visibility after commits', () => {
-		it.skip('Should not treat transaction logs as visible until successfully committed', () =>
+		it('Should not treat transaction logs as visible until successfully committed', () =>
 			dbRunner(async ({ db }) => {
 				const log = db.useLog('foo');
 				db.putSync('key1', 'value1');
