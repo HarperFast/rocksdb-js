7720fb5fb5e0b889e9d3545d7ae2acf74d0b4e7f
Chris Barber <chris@harperdb.io>
2026-02-13 03:22:34 -0600

    Remove unnecessary code

--- test/user-shared-buffer.test.ts
+++ test/user-shared-buffer.test.ts
@@ -65,18 +65,14 @@ describe('User Shared Buffer', () => {
 		it('should notify callbacks', () =>
 			dbRunner(async ({ db }) => {
 				const sharedNumber = new Float64Array(1);
-				await new Promise<void>((resolve, reject) => {
-					try {
-						const sharedBuffer = db.getUserSharedBuffer('with-callback', sharedNumber.buffer, {
-							callback() {
-								// wait so notify() returns true
-								setTimeout(() => resolve(), 100);
-							},
-						});
-						expect(sharedBuffer.notify()).toBe(true);
-					} catch (error) {
-						reject(error);
-					}
+				await new Promise<void>(resolve => {
+					const sharedBuffer = db.getUserSharedBuffer('with-callback', sharedNumber.buffer, {
+						callback() {
+							// wait so notify() returns true
+							setTimeout(() => resolve(), 100);
+						},
+					});
+					expect(sharedBuffer.notify()).toBe(true);
 				});
 			}));
 
@@ -92,21 +88,17 @@ describe('User Shared Buffer', () => {
 						encodedKey.subarray(encodedKey.start, encodedKey.end)
 					) as BufferWithDataView;
 
-					await new Promise<void>((resolve, reject) => {
-						try {
-							expect(db.listeners(key)).toBe(0);
-							const sharedBuffer = db.getUserSharedBuffer('with-callback2', sharedNumber.buffer, {
-								callback() {
-									// wait so notify() returns true
-									setImmediate(() => resolve());
-								},
-							});
-							weakRef = new WeakRef(sharedBuffer);
-							expect(sharedBuffer.notify()).toBe(true);
-							expect(db.listeners(key)).toBe(1);
-						} catch (error) {
-							reject(error);
-						}
+					await new Promise<void>(resolve => {
+						expect(db.listeners(key)).toBe(0);
+						const sharedBuffer = db.getUserSharedBuffer('with-callback2', sharedNumber.buffer, {
+							callback() {
+								// wait so notify() returns true
+								setImmediate(() => resolve());
+							},
+						});
+						weakRef = new WeakRef(sharedBuffer);
+						expect(sharedBuffer.notify()).toBe(true);
+						expect(db.listeners(key)).toBe(1);
 					});
 
 					// this can be flaky, especially when running all tests
