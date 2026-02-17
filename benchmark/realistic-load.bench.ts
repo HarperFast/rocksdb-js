e63b06bff9c20fff2200cd6acd875e7d27d12fbe
Chris Barber <chris@harperdb.io>
2026-02-17 11:06:42 -0600

    Add debug logging

--- benchmark/realistic-load.bench.ts
+++ benchmark/realistic-load.bench.ts
@@ -13,41 +13,49 @@ describe('Realistic write load with workers', () => {
 	const aaaa = Buffer.alloc(1500, 'a');
 	const ITERATIONS = 100;
 	describe('write variable records with transaction log', () => {
-		// benchmark(
-		// 	'rocksdb',
-		// 	concurrent({
-		// 		mode: 'essential',
-		// 		numWorkers: 4,
-		// 		concurrencyMaximum: 32,
-		// 		dbOptions: { disableWAL: true },
-		// 		setup(ctx: BenchmarkContext<RocksDatabase>) {
-		// 			const db = ctx.db;
-		// 			const log = db.useLog('0');
-		// 			ctx.log = log;
-		// 		},
-		// 		async bench({ db, log }) {
-		// 			for (let i = 0; i < ITERATIONS; i++) {
-		// 				await db
-		// 					.transaction((txn) => {
-		// 						const key = Math.floor(Math.random() * NUM_KEYS).toString();
-		// 						if (Math.random() < DELETE_RATIO) {
-		// 							log.addEntry(aaaa.subarray(0, 30), txn.id);
-		// 							db.removeSync(key, { transaction: txn });
-		// 						} else {
-		// 							const data = aaaa.subarray(0, Math.random() * 1500);
-		// 							log.addEntry(data, txn.id);
-		// 							db.putSync(key, data, { transaction: txn });
-		// 						}
-		// 					})
-		// 					.catch((error) => {
-		// 						if (error.code !== 'ERR_BUSY') {
-		// 							console.error('Error occurred during transaction:', error);
-		// 						}
-		// 					});
-		// 			}
-		// 		},
-		// 	})
-		// );
+		benchmark(
+			'rocksdb',
+			concurrent({
+				mode: 'essential',
+				numWorkers: 4,
+				concurrencyMaximum: 32,
+				dbOptions: { disableWAL: true },
+				setup(ctx: BenchmarkContext<RocksDatabase>) {
+					process.stdout.write(
+						'SETTING UP ROCKSDB BENCHMARK: write variable records with transaction log\n'
+					);
+					const db = ctx.db;
+					const log = db.useLog('0');
+					ctx.log = log;
+				},
+				teardown() {
+					process.stdout.write(
+						'TEARING DOWN ROCKSDB BENCHMARK: write variable records with transaction log\n'
+					);
+				},
+				async bench({ db, log }) {
+					for (let i = 0; i < ITERATIONS; i++) {
+						await db
+							.transaction((txn) => {
+								const key = Math.floor(Math.random() * NUM_KEYS).toString();
+								if (Math.random() < DELETE_RATIO) {
+									log.addEntry(aaaa.subarray(0, 30), txn.id);
+									db.removeSync(key, { transaction: txn });
+								} else {
+									const data = aaaa.subarray(0, Math.random() * 1500);
+									log.addEntry(data, txn.id);
+									db.putSync(key, data, { transaction: txn });
+								}
+							})
+							.catch((error) => {
+								if (error.code !== 'ERR_BUSY') {
+									console.error('Error occurred during transaction:', error);
+								}
+							});
+					}
+				},
+			})
+		);
 
 		benchmark(
 			'lmdb',
@@ -56,9 +64,17 @@ describe('Realistic write load with workers', () => {
 				numWorkers: 4,
 				concurrencyMaximum: 32,
 				async setup(ctx: BenchmarkContext<LMDBDatabase>) {
+					process.stdout.write(
+						'SETTING UP LMDB BENCHMARK: write variable records with transaction log\n'
+					);
 					let start = Date.now();
 					ctx.index = start;
 					ctx.lastTime = Date.now();
+				},
+				teardown() {
+					process.stdout.write(
+						'TEARING DOWN LMDB BENCHMARK: write variable records with transaction log\n'
+					);
 				},
 				async bench(ctx: BenchmarkContext<LMDBDatabase>) {
 					const { db } = ctx;
